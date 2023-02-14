91f020ff15864028a88e776df69ba7f773176a7a
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-02-14 03:04:37 -0300

    refatorando codigo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const conteiner=document.querySelector('.conteiner')
+
+
+
 const fetchapi= async ()=>{
     const dado= await fetch('https://rickandmortyapi.com/api/character')
-
     const response =await dado.json()
 console.log(response);
    return response
@@ -8,59 +11,56 @@ console.log(response);
 
     
 }
+
+const creatPerson =(p) =>{
+    
+    const person=document.createElement('div')
+    person.className='person'
+   
+
+    const personImg=document.createElement('img')
+    personImg.src=p.image
+    personImg.className='personImg'
+    person.appendChild(personImg)
+
+    const personName=document.createElement('h1')
+    personName.innerHTML= `<p class='personName'>${p.name}<p><br>`
+    person.appendChild(personName)
+    
+
+
+    const personSpecies=document.createElement('p')
+    personSpecies.innerHTML+= `<p class='personInf'>${p.species}<br>`
+    person.appendChild(personSpecies)
+
+
+    const personOrigIN=document.createElement('P')
+    personOrigIN.innerHTML+= `<p class='personInf'>${p.origin.name}<br>`
+    person.appendChild(personOrigIN)
+    
+    return person
+    
+    
+}
     
 const character=async()=>{
     const response=  await fetchapi()
-  
-
-    const conteiner=document.querySelector('.conteiner')
-
-
     response.results.map(p=>{
-        const person=document.createElement('div')
-        person.className='person'
+
+        const person=creatPerson(p)
         conteiner.appendChild(person)
-
-        const personImg=document.createElement('img')
-        personImg.src=p.image
-        personImg.className='personImg'
-        person.appendChild(personImg)
-
-        const personName=document.createElement('h1')
-        personName.innerHTML= `<p class='personName'>${p.name}<p><br>`
-        person.appendChild(personName)
-        
-
-
-        const personSpecies=document.createElement('p')
-        personSpecies.innerHTML+= `<p class='personInf'>${p.species}<br>`
-        person.appendChild(personSpecies)
-
-
-        const personOrigIN=document.createElement('P')
-        personOrigIN.innerHTML+= `<p class='personInf'>${p.origin.name}<br>`
-        person.appendChild(personOrigIN)
-        
-        
+      
 
     })
 }
 
 
 const filterCharacter= async (e)=>{
-  
     e.preventDefault()
     
-    
-    const conteiner=document.querySelector('.conteiner')
-    
-    const nameBusca=document.querySelector('.nameBusca').value
-    
     const response= await fetchapi()
-    
+    const nameBusca=document.querySelector('.nameBusca').value
     const person =document.querySelectorAll('.person')
-    
-    
     
         person.forEach(p => p.remove())
 
